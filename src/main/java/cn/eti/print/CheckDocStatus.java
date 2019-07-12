bf7534bdf83a9356dab6db54081f2ee819a75ff6
wqh0109663 <337096961@qq.com>
2019-07-12 23:29:54 +0800

    update redis connection

--- src/main/java/cn/eti/print/CheckDocStatus.java
+++ src/main/java/cn/eti/print/CheckDocStatus.java
@@ -6,17 +6,12 @@ import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
+
 //import java.util.List;
-import java.util.*;
 
 /**
  * @author wqh
@@ -25,9 +20,9 @@ import java.util.*;
 public class CheckDocStatus extends QuartzJobBean {
 
 
-    @Autowired
+//    @Autowired
 //	@Qualifier("primaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate1;
+//    protected JdbcTemplate jdbcTemplate1;
 
 //	@Autowired
 //	@Qualifier("secondaryJdbcTemplate")
@@ -46,73 +41,73 @@ public class CheckDocStatus extends QuartzJobBean {
         System.out.println("****************");
         System.out.println(new Date().toLocaleString());
         System.out.println("****************");
-
-        String selectEkpString = "select s.fd_name,s.fd_id  as FDID,e.* from sys_org_element s ,ekp_16be562dfabd8867cded e where s.fd_id = e.fd_shenQingRen1";
-        Connection con = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@//192.168.0.202:1521/topprod", "eti_sz01",
-                    "Eti2019etI04");
-
-            List<Map<String, Object>> list = jdbcTemplate1.queryForList(selectEkpString);
-            for (Map<String, Object> map : list) {
-                Object fd_shenQingDanJuBianHao = map.get("fd_shenQingDanJuBianHao");
-                System.out.println(fd_shenQingDanJuBianHao);
-                Object fd_baoXiaoRenBenYueYuS = map.get("fd_baoXiaoRenBenYueYuS");
-                Object FDID = map.get("FDID");
-                Random random = new Random(100);
-                int rand = random.nextInt();
-                System.out.println(FDID);
-                String sql = "\n" +
-                        "select fd_name from sys_org_element where fd_id = (select fd_parentid from sys_org_element where fd_id='" + FDID + "') ";
-                List<Map<String, Object>> maps = jdbcTemplate1.queryForList(sql);
-                Object o = maps.get(0).get("fd_name");
-                System.out.println(o);
-                String sql1 = " insert into  tc_oat_file(tc_oat000,tc_oat001,tc_oat003,tc_oat005,tc_oat007,tc_oatud001) values ('1','"+FDID+System.currentTimeMillis()+"','"+o+"','"+fd_shenQingDanJuBianHao+"','1','"+fd_baoXiaoRenBenYueYuS+"')";
-                System.out.println(sql1);
-                if (con != null) {
-
-                    preparedStatement = con.prepareStatement(sql1);
-                    //preparedStatement.setString(1,"1");
-                    //preparedStatement.setObject(2,System.currentTimeMillis());
-                    //preparedStatement.setObject(3,new Date());
-                    //preparedStatement.setObject(4,o);
-                    //preparedStatement.setObject(5,fd_shenQingDanJuBianHao);
-                    // preparedStatement.setObject(6,'1');
-                    //preparedStatement.setObject(7,fd_baoXiaoRenBenYueYuS);
-                    preparedStatement.execute(sql1);
-
-
-                } else {
-                    System.out.println(con + "数据连接为空");
-                }
-//			String filed = "";
-//			try {
-//				filed = getXmlFieldData(xmlval, "fd_3743154ea0d3c8");
-//				//OA单号
-//				String fd_373c0c155d1ca8 = getXmlFieldData(xmlval,"fd_373c0c155d1ca8");
-
-
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+//
+//        String selectEkpString = "select s.fd_name,s.fd_id  as FDID,e.* from sys_org_element s ,ekp_16be562dfabd8867cded e where s.fd_id = e.fd_shenQingRen1";
+//        Connection con = null;
+//        PreparedStatement preparedStatement = null;
+//        try {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            con = DriverManager.getConnection("jdbc:oracle:thin:@//192.168.0.202:1521/topprod", "eti_sz01",
+//                    "Eti2019etI04");
+//
+//            List<Map<String, Object>> list = jdbcTemplate1.queryForList(selectEkpString);
+//            for (Map<String, Object> map : list) {
+//                Object fd_shenQingDanJuBianHao = map.get("fd_shenQingDanJuBianHao");
+//                System.out.println(fd_shenQingDanJuBianHao);
+//                Object fd_baoXiaoRenBenYueYuS = map.get("fd_baoXiaoRenBenYueYuS");
+//                Object FDID = map.get("FDID");
+//                Random random = new Random(100);
+//                int rand = random.nextInt();
+//                System.out.println(FDID);
+//                String sql = "\n" +
+//                        "select fd_name from sys_org_element where fd_id = (select fd_parentid from sys_org_element where fd_id='" + FDID + "') ";
+//                List<Map<String, Object>> maps = jdbcTemplate1.queryForList(sql);
+//                Object o = maps.get(0).get("fd_name");
+//                System.out.println(o);
+//                String sql1 = " insert into  tc_oat_file(tc_oat000,tc_oat001,tc_oat003,tc_oat005,tc_oat007,tc_oatud001) values ('1','"+FDID+System.currentTimeMillis()+"','"+o+"','"+fd_shenQingDanJuBianHao+"','1','"+fd_baoXiaoRenBenYueYuS+"')";
+//                System.out.println(sql1);
+//                if (con != null) {
+//
+//                    preparedStatement = con.prepareStatement(sql1);
+//                    //preparedStatement.setString(1,"1");
+//                    //preparedStatement.setObject(2,System.currentTimeMillis());
+//                    //preparedStatement.setObject(3,new Date());
+//                    //preparedStatement.setObject(4,o);
+//                    //preparedStatement.setObject(5,fd_shenQingDanJuBianHao);
+//                    // preparedStatement.setObject(6,'1');
+//                    //preparedStatement.setObject(7,fd_baoXiaoRenBenYueYuS);
+//                    preparedStatement.execute(sql1);
+//
+//
+//                } else {
+//                    System.out.println(con + "数据连接为空");
+//                }
+////			String filed = "";
+////			try {
+////				filed = getXmlFieldData(xmlval, "fd_3743154ea0d3c8");
+////				//OA单号
+////				String fd_373c0c155d1ca8 = getXmlFieldData(xmlval,"fd_373c0c155d1ca8");
+//
+//
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            try {
+//                if (preparedStatement != null) {
+//                    preparedStatement.close();
+//                }
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//            try {
+//                if (con != null) {
+//                    con.close();
+//                }
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
 
 //			jdbcTemplate2.update(sql);
---
+++ src/main/java/cn/eti/print/util/SerializeUtil.java
@@ -0,0 +1,40 @@
+package cn.eti.print.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author 吴启欢
+ * @version 1.0
+ * @date 19-7-12 下午10:33
+ */
+public class SerializeUtil {
+    //序列化
+    public static byte[] serialize(Object object) {
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            byte[] bytes = baos.toByteArray();
+            return bytes;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    //反序列化
+    public static Object unserialize(byte[] bytes) {
+        ByteArrayInputStream bais = null;
+        try {
+            bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}

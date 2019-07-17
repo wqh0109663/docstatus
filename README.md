71beb0a64a104896fc83d65421e378a39ec0a6cd
wqh0109663 <337096961@qq.com>
2019-07-17 19:53:08 +0800

    add readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# docStatus
+## 环境
+springboot与quartz，多数据源，多种类型的数据源，redis
+## properties文件参考dev
--- src/main/java/cn/eti/print/Application.java
+++ src/main/java/cn/eti/print/Application.java
@@ -2,12 +2,16 @@ package cn.eti.print;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author 吴启欢
+ * @version 1.0
+ * @date 19-7-12 下午10:07
+ */
 @SpringBootApplication
-public class DocstatusApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DocstatusApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
